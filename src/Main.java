39f2a9594532ab9e7231dd35840698306426ad9c
IlhaPark <one1summer115@naver.com>
2024-01-12 12:10:28 +0900

    6st commit

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 5; i++) { //여섯번째 코드 중간수정
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
